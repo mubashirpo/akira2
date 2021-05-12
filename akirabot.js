cb9bb6cdb612f675ca7642c30bdb3313374d8c26
mubashirpo <81977486+mubashirpo@users.noreply.github.com>
2021-05-12 06:40:46 +0300

    Update akirabot.js

--- akirabot.js
+++ akirabot.js
@@ -6402,7 +6402,7 @@ Wa.me/966572004819
 ║┣❏ ❣️ɱ𝐔𝐁🅰𝐬𝐇𝐢𝐑❣️
 ║┣❏ 𝕞𝕌𝔹𝔸𝕤ℍ𝕚ℝ
 ║┃
-║┗━━━━*《 ${❣️ɱ𝐔𝐁🅰𝐬𝐇𝐢𝐑❣️} 》*
+║┗━━━━*《 ${namabot} 》*
 ╚═════════════════❍
 ` 
                     akira.sendMessage(from, wew, image, {quoted: { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(from ? { remoteJid: "status@broadcast" } : {}) }, message: { "imageMessage": { "url": "https://mmg.whatsapp.net/d/f/At0x7ZdIvuicfjlf9oWS6A3AR9XPh0P-hZIVPLsI70nM.enc", "mimetype": "image/jpeg", "caption": "*Subscribe Akira*\n*Github AkiRaID*\n_Support Terus Gw_", "fileSha256": "+Ia+Dwib70Y1CWRMAP9QLJKjIJt54fKycOfB2OEZbTU=", "fileLength": "28777", "height": 1080, "width": 1079, "mediaKey": "vXmRR7ZUeDWjXy5iQk17TrowBzuwRya0errAFnXxbGc=", "fileEncSha256": "sR9D2RS5JSifw49HeBADguI23fWDz1aZu4faWG/CyRY=", "directPath": "/v/t62.7118-24/21427642_840952686474581_572788076332761430_n.enc?oh=3f57c1ba2fcab95f2c0bb475d72720ba&oe=602F3D69", "mediaKeyTimestamp": "1610993486", "jpegThumbnail": fs.readFileSync('akiraganz/fake.jpg')} } }, caption: databaseuhy})
